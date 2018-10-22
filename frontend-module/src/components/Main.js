58f286e4a6b555c10d8f9bc99c4a31fdf19974e0
Evgeny <evgeny.veremeenko@gmail.com>
2018-10-23 01:45:06 +0300

    Dispatcher Pages 50%

--- frontend-module/src/components/Main.js
+++ frontend-module/src/components/Main.js
@@ -10,6 +10,7 @@ import CompanyListPage from "./pageSysAdmin/pageCompanyList";
 import EditCompanyPage from "./pageSysAdmin/pageEditCompany";
 import pageUserLogin from "./pageUserLogin/pageUserLogin";
 import PageStockList from "./pageAdmin/pageStockList";
+import EditConsigment from "./pageDispatcher/pageDispatcherEditConsigment";
 
 class MainController extends React.Component{
 	constructor(props) {
@@ -22,9 +23,10 @@ class MainController extends React.Component{
               <div>
                   <Route path="/login" component={pageUserLogin} />
                   <Route exact path="/companylist" component={CompanyListPage} /> {/*list of companies for sysadmin*/}
-                  <Route exact path="/companylist/:companyId" component={EditCompanyPage} /> {/*edit company for sysadmin*/}
                   <Route exact path="/stocks" component={PageStockList} /> {/* stock list for admin */}
-                  <Route path="/dispatcher/createorder" component={DispatcherCreateOrderPage} />
+{/*                  <Route exact path="/stocks/:stockId" component={EditCompanyPage} /> edit company for sysadmin*/}
+                  <Route exact path="/dispatcher/createorder" component={DispatcherCreateOrderPage} />
+                  <Route exact path="/dispatcher/createorder/:consigmentId" component={EditConsigment} />
               </div>
           </Router>
 	  );
--- frontend-module/src/components/pageDispatcher/pageDispatcherCreateOrderPage.js
+++ frontend-module/src/components/pageDispatcher/pageDispatcherCreateOrderPage.js
@@ -1,24 +1,50 @@
 import React, { Component } from "react";
 
 class DispatcherCreateOrderPage extends React.Component{
+    constructor(props) {
+        super(props);
+        this.changeInput = this.changeInput.bind(this);
+        this.savebtnClick = this.savebtnClick.bind(this);
+        this.state = {
+            client_id:"",
+            sender:"",
+            receiver:"",
+            status:"",
+            waybill_status:"",
+            driver:"",
+            auto:"",
+            date_departure:"",
+            date_arrival:"",
+        };
+    }
+    changeInput(event){
+        this.setState({
+            [event.target.id]: [event.target.value]
+        });
+    }
+    savebtnClick(event){
+        console.log(this.state);
+    }
+
     render(){
         return <div class="row">
             <div class="offset-md-2 col-md-8 form_clear">
                 <div class="row">
                     <div class="col-md-6">
                         <h3>Основное</h3>
-                        <small id="emailHelp" class="form-text text-muted">Компания- заказчик перевозки</small>
-                        <input type="text" class="form-control" id="order_owner" placeholder="Заказчик"/>
+                        <small class="form-text text-muted">Компания- заказчик перевозки</small>
+                        <input value={this.state.client_id} onChange={this.changeInput} type="text" class="form-control" id="client_id" placeholder="Заказчик"/>
 
-                        <small id="emailHelp" class="form-text text-muted">Адрес Отправления</small>
-                        <input type="text" class="form-control" id="order_owner" placeholder="Откуда"/>
+                        <small class="form-text text-muted">Адрес Отправления</small>
+                        <input value={this.state.sender} onChange={this.changeInput} type="text" class="form-control" id="sender" placeholder="Откуда"/>
 
-                        <small id="emailHelp" class="form-text text-muted">Адрес доставки</small>
-                        <input type="text" class="form-control" id="order_owner" placeholder="Куда"/>
+                        <small class="form-text text-muted">Адрес доставки</small>
+                        <input value={this.state.receiver} onChange={this.changeInput} type="text" class="form-control" id="receiver" placeholder="Куда"/>
 
                         <div class="form-group">
-                            <small id="emailHelp" class="form-text text-muted">Сатус заказа</small>
-                            <select class="form-control" id="exampleSelect1">
+                            <small class="form-text text-muted">Сатус заказа</small>
+                            <select onChange={this.changeInput} class="form-control" id="status">
+                                <option selected disabled>Статус</option>
                                 <option>Принят</option>
                                 <option>Отклонен</option>
                                 <option>Выполен</option>
@@ -29,68 +55,41 @@ class DispatcherCreateOrderPage extends React.Component{
                     <div class="col-md-6">
                         <h3>ТТН</h3>
 
-                        <small id="emailHelp" class="form-text text-muted">Статус</small>
-                        <select class="form-control" id="exampleFormControlSelect1">
-                            <option selected>Оформлен</option>
+                        <small class="form-text text-muted">Статус</small>
+                        <select onChange={this.changeInput} class="form-control" id="waybill_status">
+                            <option selected disabled>Статус</option>
+                            <option>Оформлен</option>
                             <option>Проверка завершена</option>
                             <option>Доставлен</option>
                         </select>
 
-                        <small id="emailHelp" class="form-text text-muted">Водитель</small>
+                        <small class="form-text text-muted">Водитель</small>
 
-                        <select class="form-control" id="exampleFormControlSelect1">
-                            <option selected>Иванов</option>
+                        <select onChange={this.changeInput} class="form-control" id="driver">
+                            <option selected disabled>Водитель</option>
+                            <option>Иванов</option>
                             <option>Петров</option>
                             <option>Тучкин</option>
                         </select>
 
-                        <small id="emailHelp" class="form-text text-muted">Автомобиль</small>
-                        <select class="form-control" id="exampleFormControlSelect1">
-                            <option selected>Volvo 10</option>
+                        <small class="form-text text-muted">Автомобиль</small>
+                        <select onChange={this.changeInput} class="form-control" id="auto">
+                            <option selected disabled>Авто</option>
+                            <option >Volvo 10</option>
                             <option>BMW 44-10</option>
                             <option>BusCry 22-13</option>
                         </select>
-                        <small id="emailHelp" class="form-text text-muted">Дата отправления</small>
-                        <input type="text" class="form-control" id="order_owner" placeholder="14.10.2015"/>
+                        <small class="form-text text-muted">Дата отправления</small>
+                        <input value={this.state.date_departure} onChange={this.changeInput} type="text" class="form-control" id="date_departure" placeholder="14.10.2015"/>
 
-                        <small id="emailHelp" class="form-text text-muted">Дата прибытия</small>
-                        <input type="text" class="form-control" id="order_owner" placeholder="15.10.2016"/>
+                        <small class="form-text text-muted">Дата прибытия</small>
+                        <input value={this.state.date_arrival} onChange={this.changeInput} type="text" class="form-control" id="date_arrival" placeholder="15.10.2016"/>
 
                     </div>
                 </div>
             </div>
             <div class="offset-md-2 col-md-8 form_clear">
-                <h3>Товарная патрия</h3>
-                <div class="row">
-                    <di class="col-md-3">
-                        <input type="text" class="form-control" placeholder="Наименование товара"/>
-                    </di>
-                    <di class="col-md-3">
-                        <select class="custom-select">
-                            <option selected>Принят</option>
-                            <option value="1">Проверен</option>
-                            <option value="2">Доставлен</option>
-                            <option value="3">Утерян</option>
-                        </select>
-                    </di>
-                    <di class="col-md-3">
-                        <input type="text" class="form-control" placeholder="Описание, количество.."/>
-                    </di>
-                    <di class="col-md-3">
-                        <button type="button" class="btn btn-info btn_fullsize">Добавить</button>
-                    </di>
-                </div>
-
-                <div class="row table_row">
-                    <div class="col-md-3">Процессор Intel</div>
-                    <div class="col-md-3">Принят</div>
-                    <div class="col-md-3">100шт</div>
-                    <div class="col-md-3"><a href="" class="btn-sm btn-dark">Удалить</a></div>
-                </div>
-            </div>
-
-            <div class="offset-md-2 col-md-8 form_clear">
-                <button class="btn btn-success btn_fullsize">Сохранить</button>
+                <a onClick={this.savebtnClick} class="btn btn-success btn_fullsize">Сохранить</a>
             </div>
         </div>
     }
