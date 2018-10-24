859da25167e42c6906cf43cc76e8e26ed638bc8c
Evgeny <evgeny.veremeenko@gmail.com>
2018-10-24 17:30:35 +0300

    Dispatcher Pages api

--- core-module/src/main/java/com/itechart/trucking/waybill/repository/WaybillRepository.java
+++ core-module/src/main/java/com/itechart/trucking/waybill/repository/WaybillRepository.java
@@ -1,16 +1,27 @@
 package com.itechart.trucking.waybill.repository;
 
+import com.itechart.trucking.auto.entity.Auto;
+import com.itechart.trucking.driver.entity.Driver;
 import com.itechart.trucking.waybill.entity.Waybill;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface WaybillRepository extends CrudRepository<Waybill, Long> {
 
     Waybill findWaybillById(Long id);
-/*    @Query("SELECT w FROM Waybill w WHERE Waybill.dateArrival=:date")
-    Waybill findCustomQueryByDate(@Param("date") String date);*/
+
+    @Query("select d From Driver d where d.id not IN (select w.driver.id FROM Waybill w where (:dDep between w.dateDeparture and w.dateArrival) or (:dArr between w.dateDeparture and w.dateArrival) and w.driver.id = :companyId GROUP BY w.driver.id)")
+    List<Driver> findCustomQueryDriverByDate(@Param("dDep") java.util.Date dDep,@Param("dArr") java.util.Date dArr,@Param("companyId") Long companyId);
+
+    @Query("select a From Auto a where a.id not IN (select w.auto.id FROM Waybill w where (:dDep between w.dateDeparture and w.dateArrival) or (:dArr between w.dateDeparture and w.dateArrival) and w.auto.id = :companyId GROUP BY w.auto.id)")
+    List<Auto> findCustomQueryAutoByDate(@Param("dDep") java.util.Date dDep, @Param("dArr") java.util.Date dArr, @Param("companyId") Long companyId);
 
 }
--- web-module/src/main/java/com/itechart/trucking/webmodule/controller/DispatcherController.java
+++ web-module/src/main/java/com/itechart/trucking/webmodule/controller/DispatcherController.java
@@ -1,13 +1,20 @@
 package com.itechart.trucking.webmodule.controller;
 
+import com.itechart.trucking.auto.entity.Auto;
 import com.itechart.trucking.company.entity.Company;
+import com.itechart.trucking.driver.entity.Driver;
 import com.itechart.trucking.order.entity.Order;
 import com.itechart.trucking.order.repository.OrderRepository;
 import com.itechart.trucking.user.repository.UserRepository;
+import com.itechart.trucking.waybill.repository.WaybillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,11 +28,60 @@ public class DispatcherController {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private WaybillRepository waybillRepository;
+
     @RequestMapping(value = "/orders/",method = RequestMethod.GET)
     public List<Order> getOrders(@ModelAttribute Order order){
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
         Company company = userRepository.findUserByUsername(name).getCompany();
         return orderRepository.findAllByCompany(company);
+    }
+
+    @RequestMapping(value = "/orders/createOrder/getdrivers",method = RequestMethod.GET)
+    public boolean getDrivers(){
+        SimpleDateFormat dateformat = new SimpleDateFormat("dd-M-yyyy");
+
+        /*Заглушка*/
+        String strdatefrom = "28-10-2018";
+        String strdateto = "29-10-2018";
+        Long companyId = 1L;
+
+        java.util.Date datedep = null;
+        java.util.Date datearr = null;
+        try {
+            datedep = dateformat.parse(strdatefrom);
+            datearr = dateformat.parse(strdateto);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        List<Driver> drivers= waybillRepository.findCustomQueryDriverByDate(datedep,datearr,companyId);
+        System.out.println(drivers.toString());
+        return true;
+    }
+
+    @RequestMapping(value = "/orders/createOrder/getAutos",method = RequestMethod.GET)
+    public boolean getAutos(){
+        SimpleDateFormat dateformat = new SimpleDateFormat("dd-M-yyyy");
+
+        /*Заглушка*/
+        String strdatefrom = "28-10-2018";
+        String strdateto = "29-10-2018";
+        Long companyId = 1L;
+
+        java.util.Date datedep = null;
+        java.util.Date datearr = null;
+        try {
+            datedep = dateformat.parse(strdatefrom);
+            datearr = dateformat.parse(strdateto);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        List<Auto> autos= waybillRepository.findCustomQueryAutoByDate(datedep,datearr,companyId);
+        System.out.println(autos.toString());
+        return true;
     }
 
     @RequestMapping(value = "/orders/{id}",method = RequestMethod.GET)
