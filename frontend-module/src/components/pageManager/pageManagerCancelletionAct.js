ea0e60a773d86ee112619ed6e16f3b615e79e1f2
aNasyaKotova <big.error@mail.ru>
2018-10-30 11:59:22 +0300

    add manager pages

---
+++ frontend-module/src/components/pageManager/pageManagerCancelletionAct.js
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+
+class ManagerCancelletionAct extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            productState: ""
+        };
+        document.title = "Акт списания"
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="offset-md-2 col-md-8 form_clear">
+                        <div className="row">
+                            <div className="col-md-5">
+                                <h3>Акт списания</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="offset-md-2 col-md-8 form_clear">
+                        <h3>Товарная партия</h3>
+                        <div className="row table_header">
+                            <div className="col-md-3">Наименование</div>
+                            <div className="col-md-3">Состоние</div>
+                            <div className="col-md-3">Количество</div>
+                            <div className="col-md-3"></div>
+                        </div>
+
+                        <div className="row table_row">
+                            <div className="col-md-3">Процессор Intel</div>
+                            <div className="col-md-3">{this.state.productState}</div>
+                            <div className="col-md-3">100шт</div>
+                            <div className="col-md-3"><span className="cancel_product">списать</span></div>
+                        </div>
+                    </div>
+                </div>
+
+                < div className="offset-md-2 col-md-8 form_clear">
+                    < button className="btn btn-success btn_fullsize"> Сохранить</button>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default ManagerCancelletionAct;
---
+++ frontend-module/src/components/pageManager/pageManagerOrders.js
@@ -0,0 +1,91 @@
+import React, {Component} from "react";
+import {render} from 'react-dom'
+import {Link} from "react-router-dom";
+
+class pageManagerOrders extends Component {
+
+    constructor(props) {
+        super(props);
+        this.getOrderList = this.getOrderList.bind(this);
+        this.renderTable = this.renderTable.bind(this);
+        this.state = {
+            orders: []
+        };
+        document.title = "Заказы"
+    }
+
+    componentDidMount() {
+         this.getOrderList().then(data => {
+             this.setState({orders:data});
+         });
+    }
+
+    /*get active orders*/
+    getOrderList() {
+        return fetch('http://localhost:8080/api/manager/orders', {method: "get"}).then(function (response) {
+            return response.json();
+        }).then(function (result) {
+            console.log(result);
+            return result;
+        });
+    }
+
+    /*render row of table ( calls from html ) */
+    renderTable(order) {
+        if (!order) return;
+        return <div className="row table_row">
+            <div className="col-md-2">{order.client.name}</div>
+            <div className="col-md-2">{order.sender.name}</div>
+            <div className="col-md-2">{order.receiver.address}</div>
+            <div className="col-md-2">{new Date(order.waybill.dateDeparture).toLocaleDateString()}</div>
+            <div className="col-md-1">{new Date(order.waybill.dateArrival).toLocaleDateString()}</div>
+            <div className="col-md-2">
+                <Link to={`/manager/edit/consignment/${order.id}`} className="manager_ref">Товарная партия</Link>
+            </div>
+            <div className="col-md-1">
+                <Link to={`/manager/edit/routelist/${order.id}`} className="manager_ref">Путевой лист</Link>
+            </div>
+        </div>
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="offset-md-1 col-md-10 superuserform_companylist">
+                    <div className="container">
+                        <div className="row table_header">
+                            <div className="col-md-2"><b>Клиент</b></div>
+                            <div className="col-md-2"><b>Название склада (отправитель)</b></div>
+                            <div className="col-md-2"><b>Название склада (получатель)</b></div>
+                            <div className="col-md-2"><b>Дата отправления</b></div>
+                            <div className="col-md-1"><b>Дата получения</b></div>
+                            <div className="col-md-2"><b></b></div>
+                            <div className="col-md-1"><b></b></div>
+                        </div>
+                        {
+                            this.state.orders.map((element)=>{
+                                return this.renderTable(element);
+                            })
+                        }
+                        <div className="table_footer">
+                            <nav aria-label="...">
+                                <ul className="pagination pagination-sm">
+                                    <li className="page-item disabled">
+                                        <a className="page-link" href="#" tabIndex="-1">1</a>
+                                    </li>
+                                    <li className="page-item"><a className="page-link" href="#">2</a></li>
+                                    <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        );
+    }
+}
+
+export default pageManagerOrders;
