19e3b84b0dd1830b79bc3b9c0685971a71261fd7
Baymurat <baymurat.orazov@mail.ru>
2018-11-27 16:16:30 +0300

    Add some text to welcome pages. Not the best option.

--- frontend-module/src/components/PagesCommon/welcomePages/anonWelcomePage.js
+++ frontend-module/src/components/PagesCommon/welcomePages/anonWelcomePage.js
@@ -4,7 +4,10 @@ export default class AnonWelcomePage extends React.Component {
 
     render() {
         return (
-            <div>Anon Welcome page</div>
+            <div className={"offset-md-3 mt-5"}>
+                <h1>Вы перешли на страницу приложения "Trucking"</h1>
+                <h2>Для того, чтобы пользоваться приложеним зарегистрируйтесь в системе</h2>
+            </div>
         );
     }
 }
